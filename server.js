4c6235f84b2a2cc4fa9d92a5f55933d6889e1f75
Sehajini Jayakody <98893191+sehajini1@users.noreply.github.com>
2024-01-30 18:17:06 +0530

    cors changed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
-app.use(cors(corsOptions))//anyone can access the backend. there is no restriction
+app.use(cors())//anyone can access the backend. there is no restriction
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
